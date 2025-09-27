c7e3dcf4f933fbd59a3caeff57efb686ea2ae895
Vince Swu <vinceswu88@gmail.com>
2025-09-28 00:22:19 +0530

    update

--- tower-defense/Base/base.gd
+++ tower-defense/Base/base.gd
@@ -6,11 +6,9 @@ var current_health: int:
 	set(health_in):
 		current_health = health_in
 		label_3d.text = str(current_health) + "/" + str(max_health)
-		
 		var red: Color = Color.RED
 		var white: Color = Color.WHITE
 		label_3d.modulate = red.lerp(white, float(current_health) / float(max_health))
-		
 		if current_health < 1:
 			get_tree().reload_current_scene()
 
@@ -20,5 +18,4 @@ func _ready() -> void:
 	current_health = max_health
 
 func take_damage() -> void:
-	print("damage dealt to base!")
 	current_health -= 1
