e80c751c0c68e0b3b007ae2bcf9a1223e9f9b1b6
Vince Swu <vinceswu88@gmail.com>
2025-09-27 17:47:52 +0530

    update

---
+++ tower-defense/RayPickerCamera/ray_picker_camera.gd
@@ -0,0 +1,8 @@
+extends Camera3D
+
+@onready var ray_cast_3d: RayCast3D = $RayCast3D
+
+func _process(_delta: float) -> void:
+	var mouse_position: Vector2 = get_viewport().get_mouse_position()
+	ray_cast_3d.target_position = project_local_ray_normal(mouse_position) * 100.0
+	ray_cast_3d.force_raycast_update()
