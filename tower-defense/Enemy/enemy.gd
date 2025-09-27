1582f60c78e53a1cbafeb8f94fd04b120e74a86e
Vince Swu <vinceswu88@gmail.com>
2025-09-27 19:40:40 +0530

    add targetting

--- tower-defense/Enemy/enemy.gd
+++ tower-defense/Enemy/enemy.gd
@@ -1,10 +1,19 @@
 extends PathFollow3D
 
 @export var speed: float = 5
-
+@export var max_health: int = 5
 @onready var base = get_tree().get_first_node_in_group("base")
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+var current_health: int:
+	set(health_in):
+		current_health = health_in
+		
+		if current_health < 1:
+			queue_free()
+
+func _ready() -> void:
+	current_health = max_health
+
 func _process(delta: float) -> void:
 	progress += delta * speed
 	if progress_ratio == 1:
