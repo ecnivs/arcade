af523cad0ebbc6622b3ed5c40eb94a873ade8302
Vince Swu <vinceswu88@gmail.com>
2025-10-03 21:47:02 +0530

    update

---
+++ retro-rpg/game_manager.gd
@@ -0,0 +1,7 @@
+extends Node
+
+var default_map_path: String
+var current_map_path: String
+
+func start_new_game():
+	current_map_path = default_map_path
--- retro-rpg/scenes/ui/title_screen.gd
+++ retro-rpg/scenes/ui/title_screen.gd
@@ -1,4 +1,5 @@
 extends Control
 
 func _on_new_game_button_pressed() -> void:
+	GameManager.start_new_game()
 	Loader.load_scene(self, "res://scenes/game.tscn")
